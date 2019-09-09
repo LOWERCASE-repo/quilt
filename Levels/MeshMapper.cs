101336291d507c6085622b7fbd3be40cf290cb38
LOWERCASE <52083329+LOWERCASE-repo@users.noreply.github.com>
2019-09-08 22:40:03 -0400

    prob gonna have to rework yet again bc heap a* optimization

---
+++ Levels/MeshMapper.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+internal class MeshMapper {
+  
+  internal static Mesh Map(NavNode start) {
+    List<Vector2> points = new List<Vector2>();
+    start.
+    // https://youtu.be/Q12sb-sOhdI?t=341
+    
+    /*
+    probably gonna have to rework navnode
+    esp with pathfinding
+    
+    */
+  }
+}
