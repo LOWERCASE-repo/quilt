923f9f3dfd0e25298fb1cab908743db969d5e288
LOWERCASE <52083329+LOWERCASE-repo@users.noreply.github.com>
2019-09-12 10:43:36 -0400

    before school

--- Levels/NavGraph.cs
+++ Levels/NavGraph.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 internal class NavGraph {
   
   internal HashSet<Vector2> positions;
-  internal NavNode start;
+  internal NavNode start; // can get rid of this for internal List<Vector2> nodes;
   internal float tolerance;
   
   internal NavGraph(NavNode start, float tolerance) {
