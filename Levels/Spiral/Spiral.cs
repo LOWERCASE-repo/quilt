deb5bad5c7d8b70de145883cf6a13a4058244152
LOWERCASE <52083329+LOWERCASE-repo@users.noreply.github.com>
2019-09-12 14:01:40 -0400

    geometric spirals

--- Levels/Spiral/Spiral.cs
+++ Levels/Spiral/Spiral.cs
@@ -6,13 +6,16 @@ internal class Spiral : LevelStyle {
   [SerializeField]
   private float radius;
   [SerializeField]
+  private float deltaAngle;
+  [SerializeField]
   private float cycles;
   
   private float totalRot;
   
   protected override Vector2 Eval(float time) {
     Quaternion rot = Quaternion.AngleAxis(totalRot * time, Vector3.forward);
-    return rot * Vector2.up * time * radius;
+    Quaternion deltaRot = Quaternion.AngleAxis(deltaAngle * totalRot * time, Vector3.forward);
+    return deltaRot * (rot * Vector2.up) * time * radius;
   }
   
   private void Start() {
